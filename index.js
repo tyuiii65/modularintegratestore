4b1e800d312c3d9cd1a956d844dbd542e2a7e5f8
tyuiii65 <sanity1999pun0suy@hotmail.com>
2024-05-08 11:30:04 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const isEven = (num) => num % 2 === 0;
+function subsets(nums) {
+  const result = [];
+  backtrack(0, []);
+  return result;
+  function backtrack(start, current) {
+    result.push([...current]);
+    for (let i = start; i < nums.length; i++) {
+      current.push(nums[i]);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+}
